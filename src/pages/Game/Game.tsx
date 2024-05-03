26bca29a75b7854bbdcca2d764dba94b251726b9
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2024-05-03 22:07:21 +0300

    board

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
+import { Chessboard, FEN, INPUT_EVENT_TYPE } from '../../../node_modules/cm-chessboard/src/Chessboard.js'
+
 export const Game = () => {
+  useEffect(() => {
+    const board = new Chessboard(document.getElementById("board"), {
+      assetsUrl: "../../../node_modules/cm-chessboard/assets",
+      position: FEN.start
+    })
+    // console.log(board)
+    board.enableMoveInput(inputHandler) // This enables the move input
+
+    function inputHandler(event) {
+      console.log(event)
+      if (event.type === INPUT_EVENT_TYPE.moveInputStarted ||
+        event.type === INPUT_EVENT_TYPE.validateMoveInput) {
+        return true // false cancels move
+      }
+    }
+    console.log(board)
+  }, [])
+
   return (
     <div>
       <p>Game</p>
+      <div id="board" />
     </div>
   );
 };
